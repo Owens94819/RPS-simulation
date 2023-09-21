63628adc1c508ba7f12e83ea415753bcc1823ba3
Johnsonmach6@gmail.com <Johnsonmach6@gmail.com>
2023-09-21 16:58:41 +0100

    ...

--- js/Rock.js
+++ js/Rock.js
@@ -7,8 +7,8 @@ export class Rock extends Obj{
       ctx,
       id: "r",
       color: "rgba(200,0,0,0)",
-      x: random(ctx.width - 60),
-      y: ctx.height - 60,
+      x: random(ctx.width - 160),
+      y: ctx.height - 160,
       objective: "s",
       backgroundImage: img
     }, ...arg);
